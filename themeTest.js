1c4cf50d567a0d715213b4ae4be273786ec4471e
Maxime Verdy <maximeverdy@gmail.com>
2021-12-18 17:10:24 +0100

    all in once

---
+++ themeTest.js
@@ -0,0 +1,14 @@
+// comment : Ctrl /
+/* comment.block.js : Alt Shift A */
+/** comment.block.documentation.js */
+
+var str = "Bonjour le monde"
+var num = 12123883
+
+var now = Date.now()
+
+var fct = () => {
+
+     return str
+
+}
